7d9048f17f8ce346f40e28c3eeaa82f56a477283
MhdMokhtar <m.aboudaher91@gmail.com>
2020-03-10 15:37:53 +0100

    Commit student files

--- SomerenDAL/Student_DAO.cs
+++ SomerenDAL/Student_DAO.cs
@@ -7,34 +7,66 @@ using System.Data.SqlClient;
 using System.Data;
 using System.Collections.ObjectModel;
 using SomerenModel;
+using System.Configuration;
 
 namespace SomerenDAL
 {
     public class Student_DAO : Base
     {
-      
-        public List<Student> Db_Get_All_Students()
+
+        private SqlConnection dbConnection;
+
+        public Student_DAO()
         {
-            string query = "SELECT student_id, student_name FROM [TABLE]";
-            SqlParameter[] sqlParameters = new SqlParameter[0];
-            return ReadTables(ExecuteSelectQuery(query, sqlParameters));
+            string connString = ConfigurationManager
+                .ConnectionStrings["pdb1920it10"]       //database name
+                .ConnectionString;
+            dbConnection = new SqlConnection(connString);
         }
 
-        private List<Student> ReadTables(DataTable dataTable)
+        public List<Student> Db_Get_All_Students()
         {
+            dbConnection.Open();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM students", dbConnection);
+            SqlDataReader reader = cmd.ExecuteReader();
             List<Student> students = new List<Student>();
 
-            foreach (DataRow dr in dataTable.Rows)
+            while (reader.Read())
             {
-                Student student = new Student()
-                {
-                    Number = (int)dr["student_id"],
-                    Name = (String)(dr["student_name"].ToString())
-                };
+                Student student = ReadStudent(reader);
                 students.Add(student);
             }
+
+            reader.Close();
+            dbConnection.Close();
+
             return students;
         }
 
+        private Student ReadStudent(SqlDataReader reader)
+        {
+            int studentID = (int)reader["studentID"];
+            string firstName = (string)reader["name"];
+            string lastName = (string)reader["lastName"];
+            DateTime birthDate = (DateTime)reader["birthdate"];
+
+            return new Student(studentID, firstName, lastName, birthDate);
+        }
+
+
+        //private List<Room> ReadTables(DataTable dataTable)
+        //{
+        //    List<Room> rooms = new List<Room>();
+
+        //    foreach (DataRow dr in dataTable.Rows)
+        //    {
+        //        Room room = new Room()
+        //        {
+        //            Number = (int)dr["roomID"]
+        //        };
+        //        rooms.Add(room);
+        //    }
+        //    return rooms;
+        //}
     }
 }
--- SomerenModel/Student.cs
+++ SomerenModel/Student.cs
@@ -7,13 +7,33 @@ using System.Threading.Tasks;
 namespace SomerenModel
 {
     public class Student
-    { 
-
+    {
         public String Name { get; set; }
-        public String LastName { get; set; }
-
+        public String lastName { get; set; }
         public int Number { get; set; } // StudentNumber, e.g. 474791
-        public DateTime BirthDate { get; set;  }
+        public DateTime BirthDate { get; set; }
 
+        public Student(int number, string name, string lastName, DateTime birthDate)
+        {
+            this.Number = number;
+            this.Name = name;
+            this.lastName = lastName;
+            this.BirthDate = birthDate;
+        }
+
+
+        public string FullName
+        {
+            get
+            {
+                return $"{Name} {lastName}";
+            }
+        }
+
+
+        public override string ToString()
+        {
+            return $"{Number}.{FullName} ({BirthDate})";
+        }
     }
 }
